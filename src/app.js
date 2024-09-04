172d4a43d62bc10548f81032063566553087664a
Trung Luu <luuductrung1234@gmail.com>
2024-09-04 16:13:45 +0700

    update

--- src/app.js
+++ src/app.js
@@ -13,11 +13,11 @@ const { auth } = require("./middlewares/authMiddleware");
 const globalErrorHandler = require("./middlewares/globalErrorHandler");
 
 const app = express();
+app.use(cors());
 
 app.set("views", path.join(rootDir, "views"));
 app.set("view engine", "ejs");
 
-app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
